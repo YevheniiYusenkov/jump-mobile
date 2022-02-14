344636a4c94b21178955278c1958174ed6215271
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-15 01:27:45 +0200

    intitialized ProfileScreen

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { LoginScreen } from '@jump/screens';
+import { LoginScreen, ProfileScreen } from '@jump/screens';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +12,7 @@ const App: React.FunctionComponent = (): JSX.Element => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='Login'>
         <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen name='Profile' component={ProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/Profile/profile.screen.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import { View, Text } from 'react-native';
+
+export const ProfileScreen: React.FunctionComponent = (): JSX.Element => {
+	return (
+		<View>
+			<Text>This is Profile Screen!</Text>
+		</View>
+	);
+};
