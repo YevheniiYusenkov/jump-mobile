7f7a02aa588430e576a8dfcce454eaaa272a44f2
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-15 02:33:44 +0200

    implemented Input in LoginScreen

--- src/screens/Login/login.screen.tsx
+++ src/screens/Login/login.screen.tsx
@@ -2,12 +2,18 @@ import * as React from 'react';
 
 import { View, Text } from 'react-native';
 
+import { Input } from '@jump/components';
+
 import { LoginScreenStyles } from './login.screen.styles';
+import { useForm } from 'react-hook-form';
 
 export const LoginScreen: React.FunctionComponent = (): JSX.Element => {
+  const { control } = useForm();
+
   return (
     <View style={LoginScreenStyles.Container}>
       <Text>This is the Login Screen!</Text>
+      <Input required={false} name='username' control={control} placeholder='Enter your username.' />
     </View>
   );
 };
