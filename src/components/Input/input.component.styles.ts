71caa3cc3ec42adcde3a1e4575b8ad9c8a5fafae
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-15 11:28:56 +0200

    changed White to Primary color

--- src/components/Input/input.component.styles.ts
+++ src/components/Input/input.component.styles.ts
@@ -2,11 +2,11 @@ import { StyleSheet } from 'react-native';
 
 import { CommonColors, CommonStyles } from '@jump/common';
 
-export const InputComponentStyles = StyleSheet.create({
+export const InputStyles = StyleSheet.create({
 	TextInput: {
 		display: 'flex',
 		color: CommonColors.Text,
-		backgroundColor: CommonColors.White,
+		backgroundColor: CommonColors.Primary,
 		paddingTop: 12,
 		paddingBottom: 12,
 		paddingLeft: 24,
