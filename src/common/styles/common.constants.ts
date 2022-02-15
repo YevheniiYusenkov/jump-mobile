f3cdd78d839666b8211e1ef785bdacf524e8c08e
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-15 03:28:21 +0200

    created constants

---
+++ src/common/styles/common.constants.ts
@@ -0,0 +1,6 @@
+export enum CommonFontSizes {
+	Small = 11,
+	Middle = 16,
+	Large = 24,
+	ExtraLarge = 32,
+}
--- src/common/styles/index.ts
+++ src/common/styles/index.ts
@@ -1,2 +1,3 @@
 export { CommonColors } from './common.colors';
+export { CommonFontSizes } from './common.constants';
 export { CommonStyles } from './common.styles';
