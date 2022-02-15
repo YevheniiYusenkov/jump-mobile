b503e67112ff10f41b1476c18fb6613687bab893
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-15 11:27:01 +0200

    update logo

---
+++ assets/index.ts
@@ -0,0 +1,6 @@
+export enum Assets {
+	LogoColored = require('./logo-colored.png'),
+	LogoWhite = require('./logo-white.png'),
+	LogoDark = require('./logo-dark.png'),
+	LogoBoxColored = require('./logo-box-colored.png'),
+}
