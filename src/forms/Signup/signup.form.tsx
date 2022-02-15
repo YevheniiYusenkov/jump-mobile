9cc70e33157b050a072e4d593670853015941c20
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-15 23:27:58 +0200

    implemented Select

--- src/forms/Signup/signup.form.tsx
+++ src/forms/Signup/signup.form.tsx
@@ -3,25 +3,26 @@ import * as React from 'react';
 import { View } from 'react-native';
 import { useForm } from 'react-hook-form';
 
-import { Input, Button } from '@jump/components';
+import { Input, Button, Select } from '@jump/components';
 
 import { SignupFormStyles } from './signup.form.styles';
 import { SignupFormData, SignupFormProps } from './signup.form.interfaces';
 
 export const SignupForm: React.FunctionComponent<SignupFormProps> = (props): JSX.Element => {
-	const { control, handleSubmit } = useForm();
+	const { control, handleSubmit, setValue } = useForm();
 
 	return (
 		<View style={[ SignupFormStyles.Container, { ...(props.style as object) } ]}>
-			<Input control={control} name='fullName' required={true} placeholder='Enter your full name' />
-			<Input control={control} name='phoneNumber' required={true} placeholder='Enter your phone number' />
-			<Input control={control} name='locality' required={true} placeholder='Enter your locality' />
-			<Input control={control} name='street' required={true} placeholder='Enter your street' />
-			<Input control={control} name='suite' required={true} placeholder='Enter your suite' />
-			<Input control={control} name='apartment' required={true} placeholder='Enter your apartment' />
-			<Input control={control} name='username' required={true} placeholder='Enter your username' />
-			<Input control={control} name='password' required={true} placeholder='Enter your password' />
-			<Input control={control} name='confirmPassword' required={true} placeholder='Confirm your password' />
+			<Select control={control} name='tariffPlan' required={false} setValue={setValue} defaultValue={'js'} />
+			<Input control={control} name='fullName' required={false} placeholder='Enter your full name' />
+			<Input control={control} name='phoneNumber' required={false} placeholder='Enter your phone number' />
+			<Input control={control} name='locality' required={false} placeholder='Enter your locality' />
+			<Input control={control} name='street' required={false} placeholder='Enter your street' />
+			<Input control={control} name='suite' required={false} placeholder='Enter your suite' />
+			<Input control={control} name='apartment' required={false} placeholder='Enter your apartment' />
+			<Input control={control} name='username' required={false} placeholder='Enter your username' />
+			<Input control={control} name='password' required={false} placeholder='Enter your password' />
+			<Input control={control} name='confirmPassword' required={false} placeholder='Confirm your password' />
 			<Button handleSubmit={handleSubmit} onSubmit={(data: SignupFormData) => {console.log(data);}} text='Submit'/>
 		</View>
 	);
