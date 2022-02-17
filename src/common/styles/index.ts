373a8e1c8ff35b7d837c77673b0bfbe77bf845d6
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-17 18:35:33 +0200

    deleted Colors, FontSizes, FontWeights

--- src/common/styles/index.ts
+++ src/common/styles/index.ts
@@ -1,4 +1,1 @@
-export { Colors } from './common.colors';
-export { FontSizes } from './common.constants';
-export { FontWeights } from './common.constants';
 export { CommonStyles } from './common.styles';
