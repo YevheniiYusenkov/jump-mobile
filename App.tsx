2db16458eeec3acddd2dda679e9577116e77f90e
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-17 18:39:41 +0200

    implemented themes feature
    Updated App to use theme.

--- App.tsx
+++ App.tsx
@@ -1,22 +1,49 @@
 import * as React from 'react';
 
+import { useEffect, useState } from 'react';
+
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { RootStackParamList } from '@jump/interfaces';
-import { LoginScreen, ProfileScreen, SignupScreen } from '@jump/screens';
+import { Theme, LightTheme, ThemeContext, ThemeContextInterface } from '@jump/themes';
+import { LoadingScreen, LoginScreen, ProfileScreen, SignupScreen } from '@jump/screens';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const App: React.FunctionComponent = (): JSX.Element => {
+  const [ isLoading, setIsLoading ] = useState<boolean>(true);
+  const [ isSigned, setIsSigned ] = useState<boolean>(false);
+  const [ theme, setTheme ] = useState<Theme>(LightTheme);
+
+  const themeData: ThemeContextInterface = {
+    theme: theme,
+    setTheme: setTheme,
+  };
+
+  useEffect(()=> {
+    setTimeout(() => {setIsLoading(false); setIsSigned(false); }, 3000);
+  }, []);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='Login'>
-        <Stack.Screen name='Login' component={LoginScreen} />
-        <Stack.Screen name='Profile' component={ProfileScreen} />
-        <Stack.Screen name='Signup' component={SignupScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ThemeContext.Provider value={themeData}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='Login'>
+          {isLoading ? (
+            <Stack.Screen name='Loading' component={LoadingScreen} />
+          ) : (
+            isSigned ? (
+              <Stack.Screen name='Profile' component={ProfileScreen} />
+            ) : (
+              <>
+                <Stack.Screen name='Login' component={LoginScreen} />
+                <Stack.Screen name='Signup' component={SignupScreen} />
+              </>
+            )
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeContext.Provider>
   );
 };
 
