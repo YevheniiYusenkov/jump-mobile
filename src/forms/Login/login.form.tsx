62661a89aab62585a7747747f949d6fb1d7dad90
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-18 09:38:34 +0200

    implemented language to LoginForm

--- src/forms/Login/login.form.tsx
+++ src/forms/Login/login.form.tsx
@@ -1,54 +1,42 @@
 import * as React from 'react';
-
-import { View } from 'react-native';
 import { useForm } from 'react-hook-form';
 
-import { faUser, faKey } from '@fortawesome/free-solid-svg-icons';
+import { faKey, faUser } from '@fortawesome/free-solid-svg-icons';
 
-import { useTheme } from '@jump/themes';
-import { Button, Input } from '@jump/components';
+import { useLanguage } from '@jump/contexts';
+import { Button, Container, ContainerType, Input } from '@jump/components';
 
-import { LoginFormStyles } from './login.form.styles';
 import { LoginFormProps } from './login.form.interfaces';
+
 
 export const LoginForm: React.FunctionComponent<LoginFormProps> = (props) => {
 	const { control, handleSubmit, formState: { errors } } = useForm();
-
-	const { theme } = useTheme();
-	const styles = LoginFormStyles(theme);
+	const { strings } = useLanguage();
 
 	return (
-		<View style={styles.Container}>
+		<Container type={ContainerType.Form}>
 			<Input
-				required='This field is required.'
+				required={strings.Required}
 				name='username' control={control}
-				placeholder='Enter your username'
+				placeholder={strings.EnterUsername}
 				icon={faUser}
 				errors={errors}
-				validate={{
-					oneLowercaseCharacter: (value) => /(?=.*[a-z])/.test(value) || 'Must have lowercase character',
-					oneUppercaseCharacter: (value) => /(?=.*[A-Z])/.test(value) || 'Must have uppercase character',
-				}}
 			/>
 
 			<Input
-				required={'This field is required.'}
+				required={strings.Required}
 				name='password'
 				control={control}
-				placeholder='Enter your password'
+				placeholder={strings.EnterPassword}
 				icon={faKey}
 				secureTextEntry={true}
 				errors={errors}
-				validate={{
-					oneLowercaseCharacter: (value) => /(?=.*[a-z])/.test(value) || 'Must have lowercase character',
-					oneUppercaseCharacter: (value) => /(?=.*[A-Z])/.test(value) || 'Must have uppercase character',
-				}}
 			/>
 
-			<View style={styles.ButtonsContainer}>
-				<Button onSubmit={props.onRequestPressed} text='Request' style={styles.RequestButton} outlined />
-				<Button onSubmit={props.onLoginSubmit} text='Login' handleSubmit={handleSubmit} style={styles.LoginButton} />
-			</View>
-		</View>
+			<Container type={ContainerType.Buttons}>
+				<Button onSubmit={props.onRequestPressed} text={strings.Request} left outlined />
+				<Button onSubmit={props.onLoginSubmit} text={strings.Login} handleSubmit={handleSubmit} right />
+			</Container>
+		</Container>
 	);
 };
