3f16ee275fb943a011584fa891e74e8535557c14
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-18 09:40:48 +0200

    exported Container

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Input } from './Input';
 export { Button } from './Button';
 export { Logo } from './Logo';
 export { Select } from './Select';
+export { Container, ContainerType } from './Container';
